48e937f9e2db1294e01ffcd522ec14f1ebf3b8c7
ElizaSitmukhanova <eliza.sit@mail.ru>
2023-08-24 16:37:31 +0300

    last version

--- js/main.js
+++ js/main.js
@@ -11,14 +11,16 @@ let tasks = [];
 
 if (localStorage.getItem('tasks')) {
     tasks = JSON.parse(localStorage.getItem('tasks'));
-
 }
+
 tasks.forEach(function(task) {
-    const cssClass = task.complete ? "task-title task-title--complete" : "task-title"; 
+    const cssClassComplete = task.complete ? "task-title task-title--complete" : "task-title"; 
+    const cssClassHonest = task.honest ? "list-group-item d-flex justify-content-between task-item highlight-honest" : 'list-group-item d-flex justify-content-between task-item';
+    const cssClassOdd = task.odd ? "list-group-item d-flex justify-content-between task-item highlight-odd" : 'list-group-item d-flex justify-content-between task-item';
 
     const taskHtml = `
-        <li id = "${task.id}" class="list-group-item d-flex justify-content-between task-item">
-					<span class="${cssClass}">${task.text}</span>
+        <li id = "${task.id}" class="${cssClassHonest} ${cssClassOdd}">
+					<span class="${cssClassComplete}">${task.text}</span>
 					<div class="task-item__buttons">
 						<button type="button" data-action="complete" class="btn-action">
 							Complete
@@ -29,14 +31,12 @@ tasks.forEach(function(task) {
 					</div>
 				</li>
         `
-
     tasksList.insertAdjacentHTML('beforeend', taskHtml);
 })
 
 checkEmptyList();
 
 form.addEventListener('submit', addTask);
-
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', completeTask);
 lastBtn.addEventListener('click', deleteLastTask);
@@ -46,44 +46,34 @@ honestBtn.addEventListener('click', checkHonest);
 
 function checkOdd(e) {
     e.preventDefault(e);
-    if (e.target.dataset.action !== 'oddtask') return
-    console.log(4585)
-
-
+    if (e.target.dataset.action !== 'oddtask') return;
     const taskItems = document.querySelectorAll('.task-item');
     for (let i = 0; i < taskItems.length; i++) {
         if (i % 2 !== 0) {
             taskItems[i].classList.toggle('highlight-odd');
-            return id=Number(taskItems.id);
+            tasks[i].odd = !tasks[i].odd;
         }
     }
-  
-    const task = tasks.find(function (task) {
-        if (task.id === id) {
-            return true;
-        }
-    })
-    task.odd = !task.odd;
     saveToLocalStorage();
     console.log(tasks);
 }
 
 function checkHonest (e) {
-    if (e.target.dataset.action !== 'honesttask') return 
-    console.log(4585)
+    if (e.target.dataset.action !== 'honesttask') return;
     const taskItems = document.querySelectorAll('.task-item');
     for (let i = 0; i < taskItems.length; i++) {
       if (i % 2 === 0) {
         taskItems[i].classList.toggle('highlight-honest');
+        tasks[i].honest = !tasks[i].honest;
       }
-    }
-    saveToLocalStorage()
+    };
+    saveToLocalStorage();
 }
 
 function deleteLastTask(e) {
-    if (e.target.dataset.action !== 'deletedlast') return 
+    if (e.target.dataset.action !== 'deletedlast') return;
     let lastTask = document.querySelector('.list-group-item:last-child');
-    const id = Number(lastTask.id) ;
+    const id = Number(lastTask.id);
 
     tasks = tasks.filter(function (task) {
         if (task.id === id) {
@@ -91,20 +81,17 @@ function deleteLastTask(e) {
         } else {
             return true
         }
-    })
-
-    console.log(tasks);
+    });
 
     lastTask.remove(); 
     checkEmptyList();
-    saveToLocalStorage()
+    saveToLocalStorage();
 }
   
 function deleteFirstTask(e) {
-    console.log(45)
-    if (e.target.dataset.action !== 'deletedfirst') return 
+    if (e.target.dataset.action !== 'deletedfirst') return;
     let firstTask = document.querySelector('.list-group-item:first-child');
-    const id = Number(firstTask.id) ;
+    const id = Number(firstTask.id);
 
     tasks = tasks.filter(function (task) {
         if (task.id === id) {
@@ -114,16 +101,14 @@ function deleteFirstTask(e) {
         }
     })
 
-    console.log(tasks);
-
     firstTask.remove(); 
     checkEmptyList();
-    saveToLocalStorage()
+    saveToLocalStorage();
 }
 
 function addTask(e) {
     e.preventDefault();
-    const textInput = input.value
+    const textInput = input.value;
 
     const newTask = {
         id: Date.now(),
@@ -131,16 +116,13 @@ function addTask(e) {
         complete: false,
         honest: false,
         odd: false,
-
     }
 
     tasks.push(newTask);
- 
-
     console.log(tasks);
-
+ 
     const cssClass = newTask.complete ? "task-title task-title--complete" : "task-title"; 
-
+     
     const taskHtml = `
         <li id = "${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
 					<span class="${cssClass}">${newTask.text}</span>
@@ -154,34 +136,18 @@ function addTask(e) {
 					</div>
 				</li>
         `
-
     tasksList.insertAdjacentHTML('beforeend', taskHtml);
+
     input.value = '';
     input.focus();
-
     checkEmptyList();
     saveToLocalStorage();
-   
-
 }
 
 function deleteTask(e) {
-    if (e.target.dataset.action !== 'delete') return 
+    if (e.target.dataset.action !== 'delete') return;
         const parentNode = e.target.closest('.list-group-item');
-        console.log(parentNode)
-
-        const id = Number(parentNode.id) ;
-
-   /*  const index = tasks.findIndex(function (task) {
-        console.log(task);
-        if (task.id === id) {
-            return true
-        }
-    })
-    console.log(index)
-
-    //delete from array with tasks
-    tasks.splice(index, 1); */
+        const id = Number(parentNode.id);
 
     tasks = tasks.filter(function (task) {
         if (task.id === id) {
@@ -192,36 +158,32 @@ function deleteTask(e) {
     })
 
     console.log(tasks);
-
     parentNode.remove();  
     checkEmptyList();
-    saveToLocalStorage()
-   
+    saveToLocalStorage();
 }
 
 function completeTask(e) {
     e.preventDefault();
-    if (e.target.dataset.action !== 'complete') return 
-        console.log('complete')
-        const parentNode = e.target.closest('.list-group-item');
-        const id = Number(parentNode.id);
-console.log(parentNode)
-        const taskIndex = tasks.findIndex(task => task.id === id);
-        const removedTask = tasks.splice(taskIndex, 1)[0];
-        tasks.push(removedTask); 
+    if (e.target.dataset.action !== 'complete') return;
+    console.log('complete')
+    const parentNode = e.target.closest('.list-group-item');
+    const id = Number(parentNode.id);
+    const taskIndex = tasks.findIndex(task => task.id === id);
+    const removedTask = tasks.splice(taskIndex, 1)[0];
+    tasks.push(removedTask);
 
-        const task = tasks.find(function(task){
-            if (task.id===id) {
-                return true
-            }
-        })
+    const task = tasks.find(function (task) {
+        if (task.id === id) {
+            return true
+        }
+    })
+    task.complete = !task.complete;
 
-        task.complete = !task.complete;
-        saveToLocalStorage()
+    saveToLocalStorage();
+    const taskTitle = parentNode.querySelector('.task-title');
+    taskTitle.classList.toggle('task-title--complete');
 
-        const taskTitle = parentNode.querySelector('.task-title');
-        taskTitle.classList.toggle('task-title--complete');
-        saveToLocalStorage()
 }
 
 function checkEmptyList() {
@@ -230,16 +192,14 @@ function checkEmptyList() {
         <div class="empty-list__title">Список дел пуст</div>
     </li>`
     tasksList.insertAdjacentHTML("afterbegin", emptyListHTML);
-    }
+    };
 
     if (tasks.length > 0) {
         const emptyListEl = document.querySelector('#emptyList');
         emptyListEl ? emptyListEl.remove() : null;
-
     }
-}
-
+};
 
 function saveToLocalStorage() {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
